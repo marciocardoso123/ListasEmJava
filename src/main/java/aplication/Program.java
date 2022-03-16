e846737c8083a55beff8e2a2e73625cff3975595
marciocardoso123 <marciodalu@gmail.com>
2022-03-16 06:45:32 -0300

    Segundo commit

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -18,7 +18,7 @@ public class Program {
         }
         //remover elementos
         System.out.println("---------------------");
-        //reova elemento q começa com a letra m
+        //reova elemento por predicado q começa com a letra m
         list.removeIf(x -> x.charAt(0) == 'M');
         for (String x : list) {
             System.out.println(x);
@@ -28,12 +28,15 @@ public class Program {
         System.out.println("Index of Bob: " + list.indexOf("Bob"));
         System.out.println("Index of Marco: " + list.indexOf("Marco"));
         System.out.println("---------------------");
+        //criar nova lista apartir de elementos de outra lista que cumpram determinado padrão
+        //primeiro converter pra Stream depois operação lambda depois converto pra list normalmente
         List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
         for (String x : result) {
-            System.out.println(x);
+            System.out.println(x);//<<gerada nova lista apenas com elementos A
         }
         System.out.println("---------------------");
-        String name = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null);
+        //crio uma String que recebe lista stream filtrando pelo caracter A e findFirst se n encontrar traz null como resposta
+        String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
         System.out.println(name);
     }
 }
